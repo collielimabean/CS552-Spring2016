a5faf0610a8e9f20211c0ba568526e7d4a6ba482
William Jen <wjen@wisc.edu>
2016-02-16 01:43:03 -0600

    Fixed overflow flag in ALU.

--- hw2/hw2_2/alu.v
+++ hw2/hw2_2/alu.v
@@ -14,7 +14,7 @@ module alu (A, B, Cin, Op, invA, invB, sign, Out, Ofl, Z);
                 ca_out, and_out, xor_out, or_out,
                 alu_out, shifter_out;
     
-    wire cout, is_add_op, overflow;
+    wire cout, is_add_op, signed_ofl;
 
     cla16 ca(.A (actual_a_operand), .B (actual_b_operand), .Cin (Cin), .S (ca_out), .Cout (cout));
     shifter shift_inst (.In (actual_a_operand), .Cnt (actual_b_operand[3:0]), .Op(Op[1:0]), .Out (shifter_out));
@@ -26,12 +26,11 @@ module alu (A, B, Cin, Op, invA, invB, sign, Out, Ofl, Z);
     assign xor_out = actual_a_operand ^ actual_b_operand;
 
     assign is_add_op = Op[2] & ~(Op[1] | Op[0]);
-    assign unsigned_ofl = actual_a_operand[15] & (actual_b_operand[15] & ~ca_out[15]);
-    assign signed_ofl = (actual_a_operand[15] & (actual_b_operand[15] & ~ca_out[15])) | (~actual_a_operand[15] & (~actual_b_operand[15] & ca_out[15]));
+    assign signed_ofl = cout & (actual_a_operand[15] ^ actual_b_operand[15]);
 
     assign alu_out = (Op[1]) ? ((Op[0] ? and_out : xor_out)) : ((Op[0]) ? or_out : ca_out);
 
     assign Out = (Op[2]) ? alu_out : shifter_out;
     assign Z = ~(|Out);
-    assign Ofl = (is_add_op) ? ((sign) ? signed_ofl : unsigned_ofl) : 1'b0;
+    assign Ofl = (is_add_op) ? ((sign) ? signed_ofl : cout) : 1'b0;
 endmodule
