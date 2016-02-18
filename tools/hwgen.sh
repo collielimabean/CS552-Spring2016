24d09063a1e088020dcdab8a4790750c59f4b821
William Jen <wjen@wisc.edu>
2016-02-18 16:39:45 -0600

    Adding HW3 files and script to grab prepackaged tarballs from Prof. Karu.

---
+++ tools/hwgen.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+    echo './hwgen.sh <hw number>'
+    exit
+fi
+
+repo_base=`dirname $0`/..
+hw_dir=$repo_base/hw$1
+
+if [ -d $hw_dir ]; then
+    echo "An existing directory for HW$1 has been detected. Please rename or remove the directory, and rerun this script."
+    exit
+fi
+
+mkdir $hw_dir
+cd $hw_dir
+
+echo "Obtaining template from Prof. Karu..."
+wget --quiet http://pages.cs.wisc.edu/~karu/courses/cs552/spring2016//handouts/verilog_code/hw$1.tar 
+
+echo "Received! Now unpacking..."
+tar xf hw$1.tar
+rm -f hw$1.tar
+cp -rf ../partners.txt .
+echo "Done!"
