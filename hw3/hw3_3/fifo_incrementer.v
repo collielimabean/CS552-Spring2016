a58376d4504b99714c48d59bbd1eb9d2e38c7b75
William Jen <wjen@wisc.edu>
2016-02-27 16:52:04 -0600

    Added fifo_incrementer.

---
+++ hw3/hw3_3/fifo_incrementer.v
@@ -0,0 +1,13 @@
+
+module fifo_incrementer(en, clk, rst, Out);
+    input en, clk, rst;
+    output [1:0] Out;
+
+    wire [1:0] inc, next_val;
+    
+    dff2 state(.d (next_val), .clk (clk), .rst (rst), .q (Out));
+
+    assign inc[0] = ~In[0];
+    assign inc[1] = In[1] ^ In[0];
+    assign next_val = (en) ? inc : Out;
+endmodule
