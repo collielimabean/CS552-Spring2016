ab21e43d026c6dc12b8ab80f1bfdec9b92641809
William Jen <wjen@wisc.edu>
2016-02-29 10:22:56 -0600

    Started fresh again, and did a proper FSM for full/empty logic.

--- hw3/hw3_3/fifo.v
+++ hw3/hw3_3/fifo.v
@@ -18,16 +18,26 @@ module fifo(/*AUTOARG*/
     output        fifo_full;
     output        err;
 
-    fifo_stephen f0[63:0](
-                .data_in(data_in),
-                .data_in_valid(data_in_valid),
-                .pop_fifo(pop_fifo),
-                .clk(clk),
-                .rst(rst),
-                .data_out(data_out),
-                .fifo_empty(fifo_empty),
-                .fifo_full(fifo_full),
-                .err(err));
+
+    wire [1:0] read, write;
+    wire full, empty;
+
+    wire [1:0] read_inc, write_inc, next_read, next_write;
+
+
+    dff read_dff[1:0](.d (next_read), .q (read), .clk (clk), .rst (rst));
+    dff write_dff[1:0](.d (next_write), .q (write), .clk (clk), .rst (rst));
+
+
+    /* dff64 [3:0] */
+    /* in, out logic */
+
+    assign next_read = (pop_fifo & ~empty) ? read_inc : read;
+    assign next_write = (data_in_valid & ~full) ? write_inc : write; 
+
+    inc ha (.In (read), .Out (read_inc));
+    inc ta (.In (write), .Out (write_inc));
+    fifo_size fs(.rst (rst), .clk(clk), .data_in_valid(data_in_valid), .pop_fifo (pop_fifo), .full (fifo_full), .empty (fifo_empty), .err (err));
 
 endmodule
 // DUMMY LINE FOR REV CONTROL :1:
