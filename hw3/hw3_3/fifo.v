9e098daca39baa912e303cb37c2317845fba634a
William Jen <wjen@wisc.edu>
2016-03-01 09:00:25 -0600

    Thought I committed the updated fifo.v. Clearly I didn't. Schematic updated, ready for submission.

--- hw3/hw3_3/fifo.v
+++ hw3/hw3_3/fifo.v
@@ -19,21 +19,27 @@ module fifo(/*AUTOARG*/
     output        err;
 
 
+    wire [255:0] entries_in, fifo_out, demux_out;
     wire [1:0] read, write;
-    wire full, empty;
-
     wire [1:0] read_inc, write_inc, next_read, next_write;
-
+    wire entry_wr_en;
 
     dff read_dff[1:0](.d (next_read), .q (read), .clk (clk), .rst (rst));
     dff write_dff[1:0](.d (next_write), .q (write), .clk (clk), .rst (rst));
+    dff64 entries[3:0](.d (entries_in), .q (fifo_out), .clk (clk), .rst(rst));
 
+    mux4to1 dout_mux[63:0] (.InA (fifo_out[63:0]), .InB (fifo_out[127:64]), .InC (fifo_out[191:128]), .InD (fifo_out[255:192]), .S (read), .Out (data_out));
+    demux1to4 din_demux[63:0](.In (data_in), .S (write), .Out0 (demux_out[63:0]), .Out1 (demux_out[127:64]), .Out2 (demux_out[191:128]), .Out3 (demux_out[255:192]));
 
-    /* dff64 [3:0] */
-    /* in, out logic */
+    assign entries_in[63:0] = (~(|demux_out[63:0]) | ~entry_wr_en) ? fifo_out[63:0] : demux_out[63:0];
+    assign entries_in[127:64] = (~(|demux_out[127:64]) | ~entry_wr_en) ? fifo_out[127:64] : demux_out[127:64];
+    assign entries_in[191:128] = (~(|demux_out[191:128]) | ~entry_wr_en) ? fifo_out[191:128] : demux_out[191:128];
+    assign entries_in[255:192] = (~(|demux_out[255:192]) | ~entry_wr_en) ? fifo_out[255:192] : demux_out[255:192];
 
-    assign next_read = (pop_fifo & ~empty) ? read_inc : read;
-    assign next_write = (data_in_valid & ~full) ? write_inc : write; 
+    assign next_read = (pop_fifo & ~fifo_empty) ? read_inc : read;
+    assign next_write = (data_in_valid & ~fifo_full) ? write_inc : write; 
+
+    assign entry_wr_en = data_in_valid & ~fifo_full;
 
     inc ha (.In (read), .Out (read_inc));
     inc ta (.In (write), .Out (write_inc));
--- hw3/hw3_3/fifo_full.v
+++
@@ -1,7 +0,0 @@
-module fifo_full(A, B, Out);
-    input [1:0] A, B;
-    output Out;
-
-    assign Out = (~A[0] & B[0] & (A[1] ~^ B[1])) | (A[0] & ~B[0] & (A[1] ^ B[1]));
-
-endmodule
