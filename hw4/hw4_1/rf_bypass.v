ed25a2fdf2bd0f41551ef3ec112fcba87321f696
William Jen <wjen@wisc.edu>
2016-03-01 14:40:41 -0600

    HW4 P1 complete.

--- hw4/hw4_1/rf_bypass.v
+++ hw4/hw4_1/rf_bypass.v
@@ -7,18 +7,26 @@ module rf_bypass (
            // Inputs
            clk, rst, read1regsel, read2regsel, writeregsel, writedata, write
            );
-   input clk, rst;
-   input [2:0] read1regsel;
-   input [2:0] read2regsel;
-   input [2:0] writeregsel;
-   input [15:0] writedata;
-   input        write;
+    input clk, rst;
+    input [2:0] read1regsel;
+    input [2:0] read2regsel;
+    input [2:0] writeregsel;
+    input [15:0] writedata;
+    input        write;
 
-   output [15:0] read1data;
-   output [15:0] read2data;
-   output        err;
+    output [15:0] read1data;
+    output [15:0] read2data;
+    output        err;
 
-   // your code
+    wire [15:0] rf_r1_out, rf_r2_out;
 
+
+    rf regfile (.clk (clk), .rst (rst), .err (err), .write (write),
+                .read1regsel (read1regsel), .read2regsel (read2regsel),
+                .writeregsel (writeregsel), .writedata (writedata),
+                .read1data (rf_r1_out), .read2data(rf_r2_out));
+
+    assign read1data = (~(|(read1regsel ^ writeregsel)) & write) ? writedata : rf_r1_out;
+    assign read2data = (~(|(read2regsel ^ writeregsel)) & write) ? writedata : rf_r2_out;
 endmodule
 // DUMMY LINE FOR REV CONTROL :1:
