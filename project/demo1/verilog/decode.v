5389a4106c66d3e969ee43d83576f64d56537ee8
Stephen Eick <eick@adelie-07.cs.wisc.edu>
2016-03-15 01:02:56 -0500

    Added sign extension; fixed bugs

--- project/demo1/verilog/decode.v
+++ project/demo1/verilog/decode.v
@@ -1,8 +1,8 @@
 
 module decode(clk, rst, Instr, WriteData,
-              ALUOp1, ALUOp2, ALUSrc, Branch, Jump,
-              If1, If2, Rf, ZeroExt, InvA, InvB, Cin, JumpReg, Set, Btr, 
-              ALUOpcode, Func, MemWrite, MemRead, 
+              ALUOp1, ALUOp2, ALUSrc, Immediate, Branch, Jump,
+              InvA, InvB, Cin, JumpReg, Set, Btr, 
+              ALUOpcode, Func, MemWrite, MemRead,
               MemToReg, Halt, Exception, Err);
 
     input [15:0] Instr, WriteData;
@@ -10,14 +10,15 @@ module decode(clk, rst, Instr, WriteData,
     output [15:0] ALUOp1, ALUOp2;
     output [2:0] ALUOpcode;
     output [1:0] Func;
-    output ALUSrc, Branch, Jump, If1, If2, Rf, ZeroExt,
+    output ALUSrc, Immediate, Branch, Jump,
            InvA, InvB, Cin,
            JumpReg, Set, Btr, MemWrite, MemRead, 
            MemToReg, Halt, Exception, Err;
     output [2:0] write_reg;
 
-    wire rf_wr_en, regdst;
- 
+    wire rf_wr_en, regdst, If1, If2, Rf, ZeroExt, optype, RfError;
+    reg OpError;
+
     ///// register file /////
     rf regfile(.clk         (clk),
                .rst         (rst),
@@ -25,13 +26,29 @@ module decode(clk, rst, Instr, WriteData,
                .read2regsel (Instr[7:5]),
                .writeregsel (write_reg),
                .write       (rf_wr_en),
+               ,writedata   (WriteData),
                .read1data   (ALUOp1),
                .read2data   (ALUOp2),
-               .err         (Err)); 
+               .err         (RfError)); 
 
     assign write_reg = (regdst) ? Instr[7:5] : Instr[4:2];
         
     assign Func = Instr[12:11];
+
+    assign Err = OpError | RfError;
+
+    always @(*) begin
+        casex({If1, If2, Jump, ZeroExt})
+            4'b1001: assign Immediate = {11{0}, Instr[4:0]};
+            4'b1000: assign Immediate = {11{Instr[4]}, Instr[4:0]};
+            4'b0101: assign Immediate = {8{0}, Instr[7:0]};
+            4'b0100: assign Immediate = {8{Inst[7]}, Instr[7:0]};
+            4'b0010: assign Immediate = {5{Instr[10]}, Instr[10:0]};
+            default: assign OpError = 1'b1;
+
+
+        endcase
+    end
 
     ///// Control unit //////
     control_unit cu(.opcode     (Instr[15:11]),
