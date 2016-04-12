235513e445e02070a6b82c47370a892e2e785717
William Jen <wjen@wisc.edu>
2016-04-12 00:14:54 -0500

    Fixed syntax in h5p1.

--- hw5/hw5_1/p1.asm
+++ hw5/hw5_1/p1.asm
@@ -1,13 +1,13 @@
 // H5, P1 - Forwarding
 
-lbi r0, 50 ; r0 = 50
-lbi r1, 25 ; r1 = 25
-stu r0, r0, 0   ; M[50] = 50
+lbi r0, 50       // r0 = 50
+lbi r1, 25       // r1 = 25
+stu r0, r0, 0    // M[50] = 50
 
 
-add r2, r0, r1  ; r2 = 75
-add r0, r2, r2  ; r0 = 150
-ld r3, r0, 0    ; r3 = 50
-xor r4, r3, r0  ; r4 = 50 ^ 150 = 164
+add r2, r0, r1   // r2 = 75
+add r0, r2, r2   // r0 = 150
+ld r3, r0, 0     // r3 = 50
+xor r4, r3, r0   // r4 = 50 ^ 150 = 164
 
 halt
