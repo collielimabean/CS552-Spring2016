14c17c274665658c7a301cf509c98f9c632ade19
William Jen <wjen@wisc.edu>
2016-04-12 00:41:02 -0500

    Did h5p2.

--- hw5/hw5_2/p2.asm
+++ hw5/hw5_2/p2.asm
@@ -1,1 +1,12 @@
-// Write your assembly program for Problem 2 (a) here.
+
+lbi r1, 10     // change this to modify branch outcome
+
+beqz r1, 2       // if r1 == 0, go to load area
+halt
+xor r0, r0, r0
+xor r1, r1, r1
+ld r1, r1, 0
+ld r1, r1, 0
+ld r1, r1, 0
+ld r1, r1, 0
+halt
